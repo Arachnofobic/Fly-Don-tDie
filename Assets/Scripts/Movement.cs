b2cabfd0400003ae51cd876f10b0e8373c5a0d83
Arachnofobic <kamilfik97@gmail.com>
2021-03-22 09:55:15 +0100

    Added Rocket Controll
    Added Key bindings to controll rocket. set up values of thrust and totation thrust. Fixed bug with physics system

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float thrust;
+    [SerializeField] float rotationThrust;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+            rb.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+        }
+    }
+    
+    void ProcessRotation()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        
+        else if (Input.GetKey(KeyCode.D))
+        {
+          ApplyRotation(-rotationThrust);
+        }
+
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;   //freezing rotation to manualy rotate 
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;  //unfreezing rotation so physics system can take over 
+    }
+}
