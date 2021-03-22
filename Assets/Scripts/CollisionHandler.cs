6b641ca70e8120fe48b83f80cd238743b42b997d
Arachnofobic <kamilfik97@gmail.com>
2021-03-22 12:56:14 +0100

    SFX, Particles
    Added sfx and particles

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float levelLoadDealay = 2;
+    [SerializeField] AudioClip finish;
+    [SerializeField]  AudioClip crash;
+    
+    [SerializeField] ParticleSystem finishParticles;
+    [SerializeField] ParticleSystem crashParticles;
+
+    AudioSource audioSource;
+
+
+    bool isTransitioning = false;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (isTransitioning)
+        {
+            return;
+        }
+
+        
+            switch (other.gameObject.tag)
+            {
+                case "Friendly":
+                    Debug.Log("This thing is friendly");
+                    break;
+                
+                case "Finish":
+                    StartSuccesSequence();
+                    break;
+                
+                case "Fuel":
+                    Debug.Log("You've picked up fuel");
+                    break;
+                
+                default:
+                    Debug.Log("Welp. You blew up :/");
+                    StartCrashSequence();
+                    break;
+            }
+    }
+
+
+
+    void StartSuccesSequence()
+    {
+        isTransitioning = true;
+        
+        audioSource.Stop();
+        audioSource.PlayOneShot(finish);
+        finishParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke ("LoadNextLevel",levelLoadDealay); 
+        
+    }
+    
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crash);
+        crashParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke ("ReloadLevel",levelLoadDealay);
+        
+    }
+    
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex+ 1;
+        if(nextSceneIndex==SceneManager.sceneCountInBuildSettings)
+        {
+
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,13 +3,23 @@ using UnityEngine.PlayerLoop;
 
 public class Movement : MonoBehaviour
 {
+    //PARAMETERS - for tuning, typically set in the editor;
+    //CACHE - e.g. references for readability or speed;
+    //STATE - private instance (member) variable;
 
-    [SerializeField] Rigidbody rb;
-    [SerializeField] float thrust;
+    [SerializeField]  AudioClip mainEngine;
+    [SerializeField] float thrust = 100;
     [SerializeField] float rotationThrust;
+    
+    Rigidbody rb;
+    AudioSource audioSource;
+    
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+       
     }
     
     void Update()
@@ -25,6 +35,15 @@ public class Movement : MonoBehaviour
         {
             
             rb.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+            
+            if(!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
     
@@ -49,4 +68,6 @@ public class Movement : MonoBehaviour
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rb.freezeRotation = false;  //unfreezing rotation so physics system can take over 
     }
+    
+  
 }
